79d709c2310c14bad9b985dff63198c1d4bd3d38
fkuner <39162698+fkuner@users.noreply.github.com>
2019-04-02 18:34:12 +0800

    增加 了Money类，备忘录写了点

---
+++ Manager/ViewModels/BlankPage1ViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Core;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace Manager.ViewModels
+{
+    class BlankPage1ViewModel
+    {
+        
+    }
+}
--- Manager/rootFrame/PointerPressed.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace rootFrame
-{
-    internal class PointerPressed
-    {
-    }
-}
