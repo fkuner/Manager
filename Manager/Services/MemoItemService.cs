ca8a96d03f52f4db32bd7a0a426e50b78d28f4af
tangyijun <2537196761@qq.com>
2019-04-06 20:56:26 +0800

    AddServices

--- Manager/Services/MemoItemService.cs
+++ Manager/Services/MemoItemService.cs
@@ -5,24 +5,78 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
 
 namespace Manager.Services
 {
     public class MemoItemService : IMemoItemService
     {
+        //缺一个初始化命令,坤哥，等会儿你试的时候一定要App.xaml.cs加载中给它初始化
+        public void InitializeDatabase()
+        {
+            using (SqliteConnection db = new SqliteConnection("Filename=sqliteData.db"))
+            {
+                db.Open();
+
+                String tableCommand = "CREATE TABLE IF NOT EXISTS MemoItemTable " +
+                    "(ID INTEGER PRIMARY KEY NOT NULL," +
+                    "Content NVACHAR(2048) NULL)";
+
+                SqliteCommand createTable = new SqliteCommand(tableCommand, db);
+
+                createTable.ExecuteReader();
+            }
+        }
+
         public void AddAsync(MemoItem memoItem)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            //添加命令
+            using (SqliteConnection db = new SqliteConnection("Filename=sqliteData.db"))
+            {
+                db.Open();
+
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+
+                insertCommand.CommandText = "INSERT INTO MemoItemTable VALUES (@id,@content);";
+                insertCommand.Parameters.AddWithValue("@id", memoItem.ID);
+                insertCommand.Parameters.AddWithValue("@content", memoItem.Content);
+
+                insertCommand.ExecuteReader();
+                db.Close();
+            }
+
         }
 
         public void DeleteAsync(MemoItem memoItem)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            //删除命令
+            using (SqliteConnection db = new SqliteConnection("Filename=sqliteData.db"))
+            {
+                db.Open();
+
+                SqliteCommand deleteCommand = new SqliteCommand("DELETE FROM MemoItemTable WHERE ID = @id;");
+                deleteCommand.Parameters.AddWithValue("@id", memoItem.ID);
+
+                deleteCommand.ExecuteReader();
+                db.Close();
+            }
         }
 
         public int FindMemoItem(List<MemoItem> MemoItems, MemoItem memoItem)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            //这里不涉及数据库操作吗？感觉这个接口写的有点问题，坤哥等会儿你自己改一下
+            foreach (MemoItem m in MemoItems)
+            {
+                if(m.ID == memoItem.ID)
+                {
+                    return m.ID;
+                }
+            }
+            return -1;
         }
 
         //private MemoItem _memoItem;
@@ -30,16 +84,20 @@ namespace Manager.Services
 
         public List<MemoItem>  ListAsync()
         {
-            List<MemoItem> MemoItems = new List<MemoItem>();
-
-            for (int i=0;i<5;i++)
+            List<MemoItem> List = new List<MemoItem>();
+            using (SqliteConnection db = new SqliteConnection("Filename=sqliteData.db"))
             {
-                MemoItem memoItem = new MemoItem();
-                memoItem.Content=i.ToString();
-                MemoItems.Add(memoItem);
+                db.Open();
+                SqliteCommand selectCommand = new SqliteCommand
+            ("SELECT * from MemoItemTable", db);
+                SqliteDataReader query = selectCommand.ExecuteReader();
+                while (query.Read())
+                {
+                    List.Add(new MemoItem { ID=(int)query["ID"],Content=query["Content"].ToString()});
+                }
+                db.Close();
             }
-            return MemoItems;
-            throw new NotImplementedException();
+            return List;
         }
     }
 }
