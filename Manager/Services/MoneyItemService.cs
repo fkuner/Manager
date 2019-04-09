bc537b2750f9826b295be8c1c5419bf36e520cda
oricholcos <1363242896@qq.com>
2019-04-09 14:56:22 +0800

    MoneyItemService接口的实现

--- Manager/Services/MoneyItemService.cs
+++ Manager/Services/MoneyItemService.cs
@@ -44,7 +44,7 @@ namespace Manager.Services
                 SqliteDataReader query = selectCommand.ExecuteReader();
                 while (query.Read())
                 {
-                    List.Add(new MoneyItem { Id = Convert.ToInt32(query["Id"]), DateCreated = Convert.ToDateTime(query["DateCreated"].ToString()), Title = query["Title"].ToString(), Text = query["Text"].ToString() });
+                    List.Add(new MoneyItem { Id = Convert.ToInt32(query["Id"]), ConsumeTime = Convert.ToDateTime(query["ConsumeTime"].ToString()), Event = query["Event"].ToString(), Amount = Convert.ToDouble(query["Amount"]) , CoverImage = query["CoverImage"].ToString() });
                 }
                 db.Close();
             }
@@ -53,12 +53,39 @@ namespace Manager.Services
 
         public void AddAsync(MoneyItem moneyItem)
         {
-            throw new NotImplementedException();
+            using (SqliteConnection db = new SqliteConnection("Filename=sqliteData.db"))
+            {
+                db.Open();
+
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+
+                insertCommand.CommandText = "INSERT INTO MoneyItemTable VALUES (@id,@time,@event,@amount,@cover);";
+                insertCommand.Parameters.AddWithValue("@id", moneyItem.Id);
+                insertCommand.Parameters.AddWithValue("@time", moneyItem.ConsumeTime);
+                insertCommand.Parameters.AddWithValue("@event", moneyItem.Event);
+                insertCommand.Parameters.AddWithValue("@amount", moneyItem.Amount);
+                insertCommand.Parameters.AddWithValue("@cover", moneyItem.CoverImage);
+                insertCommand.ExecuteReader();
+                db.Close();
+            }
         }
 
         public void DeleteAsync(MoneyItem moneyItem)
         {
-            throw new NotImplementedException();
+            using (SqliteConnection db = new SqliteConnection("Filename=sqliteData.db"))
+            {
+                db.Open();
+
+                SqliteCommand deleteCommand = new SqliteCommand("DELETE FROM MoneyItemTable WHERE Id = @id AND ConsumeTime = @time AND Event = @event AND Amount = @amount AND CoverImage = @cover;");
+                deleteCommand.Parameters.AddWithValue("@id", moneyItem.Id);
+                deleteCommand.Parameters.AddWithValue("@time", moneyItem.ConsumeTime);
+                deleteCommand.Parameters.AddWithValue("@event", moneyItem.Event);
+                deleteCommand.Parameters.AddWithValue("@amount", moneyItem.Amount);
+                deleteCommand.Parameters.AddWithValue("@cover", moneyItem.CoverImage);
+                deleteCommand.ExecuteReader();
+                db.Close();
+            }
         }
     }
 }
